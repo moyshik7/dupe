63712fa7c13544e9addc4b285d4019d6dfcb451c
Talat Mahmud Oyshik <58624651+moyshik7@users.noreply.github.com>
2020-10-01 13:14:32 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const token = ".";
 const client = new Discord.Client();
-const cList = require("./commands.json");
+const cList = require("./res/commands.json");
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`);
