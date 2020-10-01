64348495098b0cc8e3e3409c216eafebc3d09f12
Talat Mahmud Oyshik <58624651+moyshik7@users.noreply.github.com>
2020-10-01 18:10:06 +0600

    Finally fixed bugs

---
+++ res/reference.js
@@ -0,0 +1,40 @@
+const Discord = require("discord.js");
+
+const anime = require ("./anime.json");
+const color = require("./color.json");
+const hentai = require("./hentai.json");
+const neko = require("./neko.json");
+const wallpaper = require("./wallpaper.json");
+
+const ref = {
+	embed : (message, Title, Description, ImageArray) => {
+		let a = new Discord.MessageEmbed()
+			.setColor(ref.randArr(color))
+			.setTitle(Title)
+			.setDescription(Description)
+			.setImage(ref.randArr(ImageArray))
+		message.channel.send(a);
+	},
+    formatArr: (givenArray) => {
+        let finale = [];
+        for (let x = 0; x < givenArray.length; x++) {
+            let p = givenArray[x];
+            if (p != "" && p != null && p != undefined) {
+                p = ref.formatStr(p);
+                if (p.length >= 1) {
+                    finale.push(p);
+                }
+            }
+        }
+        return (finale);
+    },
+    formatStr: (givenString) => {
+        let a = givenString.toLowerCase();
+        a = a.trim();
+        return (a);
+    },
+    randArr: (givenArray) => {
+        return (givenArray[Math.floor(Math.random() * givenArray.length)]);
+    }
+}
+module.exports = ref;
