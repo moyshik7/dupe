3fe52537ec26953d8ac7c8ef1f280f0f1d91e42b
Talat Mahmud Oyshik <58624651+moyshik7@users.noreply.github.com>
2020-12-06 11:32:07 +0600

    update reference.js

--- res/reference.js
+++ res/reference.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const fetch = require('node-fetch');
 const qs = require('querystring');
 
-const anime = require("./anime.json");
+//const anime = require("./anime.json");
 const color = require("./color.json");
 
 const ref = {
@@ -25,27 +25,36 @@ const ref = {
     },
     embed: (message, Title, Description, ImageArray) => {
         let a;
-        if (ImageArray == null || ImageArray == undefined) {
+        if (typeof(ImageArray) === "string") {
             a = new Discord.MessageEmbed()
                 .setColor(ref.randArr(color))
                 .setTitle(Title)
-                .setDescription(Description)
-        } else {
-            if (typeof(ImageArray) === "string") {
-                a = new Discord.MessageEmbed()
-                    .setColor(ref.randArr(color))
-                    .setTitle(Title)
-                    .setDescription(Description)
-                    .setImage(ImageArray)
-            } else if (typeof(ImageArray) === "object") {
-                a = new Discord.MessageEmbed()
-                    .setColor(ref.randArr(color))
-                    .setTitle(Title)
-                    .setDescription(Description)
-                    .setImage(ref.randArr(ImageArray))
-            } else {}
-        }
+                .setImage(ImageArray)
+                .setFooter("Support us on Patreon", "https://i.imgur.com/RKVSeIv.png")
+        } else if (typeof(ImageArray) === "object") {
+            a = new Discord.MessageEmbed()
+                .setColor(ref.randArr(color))
+                .setTitle(Title)
+                .setImage(ref.randArr(ImageArray))
+                .setFooter("Support us on Patreon", "https://i.imgur.com/RKVSeIv.png")
+        } else {}
         message.channel.send(a);
+    },
+    embedDes : (message,Title,Description,Footer) => {
+    	let emb;
+    	if(Footer == false || Footer == undefined){
+    		emb = new Discord.MessageEmbed()
+    			.setColor(ref.randArr(color))
+    			.setTitle(Title)
+    			.setDescription(Description)
+    	}else{
+    		emb = new Discord.MessageEmbed()
+    			.setColor(ref.randArr(color))
+    			.setTitle(Title)
+    			.setDescription(Description)
+    			.setFooter(Footer)
+    	}
+    	message.channel.send(emb);
     },
     formatArr: (givenArray) => {
         let finale = [];
@@ -64,6 +73,9 @@ const ref = {
         let a = givenString.toLowerCase();
         a = a.trim();
         return (a);
+    },
+    randBool: () => {
+    	return(Math.random() < 0.5);
     },
     randArr: (givenArray) => {
         return (givenArray[Math.floor(Math.random() * givenArray.length)]);
