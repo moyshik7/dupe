99ee9c5e72c0d1291f8b6b0622431f6c87f434f3
Talat Mahmud Oyshik <58624651+moyshik7@users.noreply.github.com>
2020-12-15 13:25:25 +0600

    finalized

--- index.js
+++ index.js
@@ -9,23 +9,31 @@ const ref = require("./res/reference");
 
 client.once("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setActivity('Apex Legends');
+    client.user.setActivity('.h or .help', {
+        type: "LISTENING"
+    });
+    client.user.setStatus('idle');
 });
 client.on("message", (message) => {
-    if (message.author.bot) return;
-    let msg = message.content;
-    if (msg[0] === token) {
-        let command = msg.slice(1);
-        command = ref.formatArr(command.split(" "));
-        args = command.slice(1);
-        command = command[0];
-        if (cList.indexOf(command) != -1) {
-            let myf = commands[command];
-            myf(message, args);
+    if (message.author.bot) {
+        return(false);
+    } else {
+        let msg = message.content;
+        if (msg[0] === token) {
+            let command = msg.slice(1);
+            command = ref.formatArr(command.split(" "));
+            args = command.slice(1);
+            command = command[0];
+            if (cList.indexOf(command) != -1) {
+                let myf = commands[command];
+                myf(message, args);
+            }
         }
     }
 });
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN); //Login using the environment variable
+
+
 server.all('/', (req, res) => {
     res.send('Plubin is up');
 });
