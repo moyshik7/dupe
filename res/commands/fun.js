1376fa3ca2d8db1e7d0e52e852e1fefeb532793b
Talat Mahmud Oyshik <58624651+moyshik7@users.noreply.github.com>
2020-12-20 20:26:53 +0600

    fixed the damn shit again

--- res/commands/fun.js
+++ res/commands/fun.js
@@ -4,7 +4,8 @@ const ref = require('./../reference.js');
 let app = {
     pp: (message, args) => {
         try {
-            let ym = ref.randInt(0, 12)
+            let ym = ref.randInt(0, 12)"
+            let yp = "";
             if (ym == 0) {
                 yp = "{(•)}\n\nIt dosen't exist bish";
                 ref.embedDes(message, "PP measurement :", yp, "Women are also part of our community");
