d099f471d69cdc186ed895e920bddf9e51cc3283
moyshik7 <moyshik7@gmail.com>
2021-01-01 18:33:59 +0600

    Added server print

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ client.on("message", (message) => {
                 args = command.slice(1);
                 command = command[0];
                 if (cList.indexOf(command) != -1) {
-                    console.log(`${command} requested by ${message.author.tag}`);
+                    console.log(`${command} requested by ${message.author.tag} at server ${message.guild}`);
                     bot.bd.message = message;
                     bot.bd.args = args;
                     bot.bd.client = client;
