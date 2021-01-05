32c380d265acad29764fd845ef528bd10ee9e6ef
moyshik7 <moyshik7@gmail.com>
2021-01-05 22:09:13 +0600

    trying to add a webhook

--- index.js
+++ index.js
@@ -62,6 +62,10 @@ dbl.on('error', e => {
 server.all('/', (req, res) => {
     res.send('Plubin is up');
 });
-server.listen(PORT, () => {
+server.all('/favicon.ico' , (req,res) => {
+    res.send('Not provided');
+});
+server.listen(PORT, (a) => {
     console.log(`Server is up on port : ${PORT}`);
+    console.log(a);
 });
