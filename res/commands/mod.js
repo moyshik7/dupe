fb1734c8f829dd2ce6754eeae7d61b12e73f14a7
moyshik7 <moyshik7@gmail.com>
2021-01-06 11:15:13 +0600

    Im the biggest idiot to step on earth

--- res/commands/mod.js
+++ res/commands/mod.js
@@ -36,13 +36,8 @@ let app = {
 		    reason = "No valid reason specified";
 		}
 		let kName = k.user.username;
-		k.kick()
-		    .then( () => {
-		        ref.embedDes(message, `${kName} was kicked`,`__REASON :__  ${reason}`, "User removed from server");
-		    })
-		    .error((err) => {
-			console.log(err);
-		    })
+		k.kick();
+		ref.embedDes(message, `${kName} was kicked`,`__REASON :__  ${reason}`, "User removed from server");
 	    }else{
 		ref.embedDes(message, "Permission Error","I don\'t have the __KICK USER__ permission", "No one kicked");
 	    }
