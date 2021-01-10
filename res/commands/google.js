d007f228d6ba1ac4a0811e788858239725ccae8f
moyshik7 <moyshik7@gmail.com>
2021-01-10 23:24:12 +0600

    changed dimention

--- res/commands/google.js
+++ res/commands/google.js
@@ -1,7 +1,7 @@
 const puppeteer = require("puppeteer");
 const Discord = require("discord.js");
 const imgur = require("./../imgur");
-const ref = require("./../reference");
+let ref = require("./../reference");
 
 const ref = require("./../reference.js");
 
@@ -16,7 +16,7 @@ let app = {
 		let link = `https://www.google.com/search?q=${a}`;
                 let browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox']});
 		const page = await browser.newPage();
-		await page.setViewport({ width: 1080, height: 720});
+		await page.setViewport({ width: 1920, height: 1080});
 		await page.goto(link);
 		await page.waitForTimeout(1000);
 		await page.screenshot().then((buff) => {
