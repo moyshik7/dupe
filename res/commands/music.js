7f58e356154864bc472ff4909aa27a10d11bbb58
moyshik7 <moyshik7@gmail.com>
2021-01-18 17:13:54 +0600

    Trying to add music

--- res/commands/music.js
+++ res/commands/music.js
@@ -5,23 +5,35 @@ const ytdl = require("ytdl-core");
 
 const app = {
     Play: (message,args) => {
-        let vc = message.member.voice.channel;
-	if(vc){
-	    vc.join().then(connection => {
-	        const stream = ytdl('https://youtu.be/BVwAVbKYYeM', { filter: 'audioonly' });
-	        const dispatcher = connection.play(stream);
-                dispatcher.on('finish', () => vc.leave());
-	    }).catch(err => {console.log(err)});
-	} else {
-	    //Join a voice channel first
+	try{
+            let vc = message.member.voice.channel;
+	    let selfVC = message.guild.me.voice.channel;
+	    
+	    if(vc && !selfVC){
+	        vc.join().then(connection => {
+	            const stream = ytdl('https://youtu.be/BVwAVbKYYeM', { filter: 'audioonly' });
+	            const dispatcher = connection.play(stream);
+                    dispatcher.on('finish', () => vc.leave());
+	        }).catch(err => {console.log(err)});
+	    } else if (selfVC != vc) {
+	        message.channel.send("Sorry I am already connected to another channel");
+	    } else{
+		//hshdhdg
+	    }
+	}catch(err){
+	    console.log(err);
 	}
     },
     Join: (message,args) => {
-	let vc = message.member.voice.channel;
-	if(vc) {
-	    vc.join();
-	} else {
-	    //Connect to a voice channel first
+	try{
+	    let vc = message.member.voice.channel;
+	    if(vc) {
+	        vc.join();
+	    } else {
+	        //-£--£
+	    }
+	} catch(err){
+	    console.log(err);
 	}
     }
 }
