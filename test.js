b2a38d15aa33f195621293146c114fe8e1b67452
moyshik7 <moyshik7@gmail.com>
2021-01-19 21:32:28 +0600

    added test script

---
+++ test.js
@@ -0,0 +1,14 @@
+
+// Testing Dupe / Plubin
+// Script:
+// npm install
+// node test.js
+
+const Discord = require("discord.js");
+const Express = require("express");
+
+if(Discord && Express){
+    console.log("Test successful");
+}else{
+    console.log("Tesr failed");
+}
