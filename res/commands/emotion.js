d551cda6dd229ebfcfe054b83bd3ed4786d09afe
moyshik7 <moyshik7@gmail.com>
2021-02-03 14:29:18 +0600

    added a perfect (seems) template for emotion commands

--- res/commands/emotion.js
+++ res/commands/emotion.js
@@ -1,9 +1,28 @@
 
 const Discord = require("discord.js");
+const ref = require("./../reference.js");
+const nLife = require("nekos.life");
+const neko = new nLife();
 
 const app = {
-    Pat : async () => {
+    Pat : async (message,args) => {
 	try{
+	    let user1;
+	    let user2;
+	    if(!args.length){
+		user1 = "Plubin"
+		user2 = message.author.user.username;
+	    }else if(!message.mentions.members.first() && args.length){
+		user1 = message.author.user.username;
+		user2 = args.join(" ");
+	    }else{
+		user1 = message.author.user.username;
+		user2 = message.mentions.members.first().user.username;
+	    }
+	    let text = `${user1} pats ${user2}'s head lightly`;
+	    let uri = await neko.sfw.hug();
+	    uri = uri.url;
+	    ref.embed(message,text, "Please vote us. (Use .vote)", uri);
 	} catch (err){
 	    console.log(err);
 	}
