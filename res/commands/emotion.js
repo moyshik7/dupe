0e31c511c5ee97a7c4cb822137d0d5f64281f504
moyshik7 <moyshik7@gmail.com>
2021-02-03 20:54:45 +0600

    added hug and kiss in emotion.js

--- res/commands/emotion.js
+++ res/commands/emotion.js
@@ -1,34 +1,74 @@
-
 const Discord = require("discord.js");
 const ref = require("./../reference.js");
 const nLife = require("nekos.life");
 const neko = new nLife();
 
 const app = {
-    Pat : async (message,args) => {
-	try{
-	    let user1;
-	    let user2;
-	    if(!args.length){
-		user1 = "Plubin"
-		user2 = message.author.username;
-	    }else if(!message.mentions.members.first() && args.length){
-		user1 = message.author.username;
-		user2 = args.join(" ");
-	    }else if(message.mentions.members.first()){
-		user1 = message.author.username;
-		user2 = message.mentions.members.first().user.username;
-	    }else{
-		user1 = "Plubin";
-		user2 = message.author.username;
-	    }
-	    let text = `${user1} pats ${user2}'s head lightly`;
-	    let uri = await neko.sfw.pat();
-	    uri = uri.url;
-	    ref.embed(message,text, "Please vote us. (Use .vote)", uri);
-	} catch (err){
-	    console.log(err);
-	}
+    Hug: async (message, args) => {
+        try {
+            let user1;
+            let user2;
+            if (!args.length) {
+                user1 = "Plubin"
+                user2 = message.author.username;
+            } else if (!message.mentions.members.first() && args.length) {
+                user1 = message.author.username;
+                user2 = args.join(" ");
+            } else {
+                user1 = message.author.username;
+                user2 = message.mentions.members.first().user.username;
+            }
+            let text = `${user1} hugged ${user2}`;
+            let uri = await neko.sfw.kiss();
+            uri = uri.url;
+            ref.embed(message, text, "", uri);
+        } catch (err) {
+            console.log(err);
+        }
+    },
+    Kiss: async (message, args) => {
+        try {
+            let user1;
+            let user2;
+            if (!args.length) {
+                user1 = "Plubin"
+                user2 = message.author.username;
+            } else if (!message.mentions.members.first() && args.length) {
+                user1 = message.author.username;
+                user2 = args.join(" ");
+            } else {
+                user1 = message.author.username;
+                user2 = message.mentions.members.first().user.username;
+            }
+            let text = `${user1} kissed ${user2}`;
+            let uri = await neko.sfw.kiss();
+            uri = uri.url;
+            ref.embed(message, text, "", uri);
+        } catch (err) {
+            console.log(err);
+        }
+    },
+    Pat: async (message, args) => {
+        try {
+            let user1;
+            let user2;
+            if (!args.length) {
+                user1 = "Plubin"
+                user2 = message.author.username;
+            } else if (!message.mentions.members.first() && args.length) {
+                user1 = message.author.username;
+                user2 = args.join(" ");
+            } else {
+                user1 = message.author.username;
+                user2 = message.mentions.members.first().user.username;
+            }
+            let text = `${user1} pats ${user2}'s head lightly`;
+            let uri = await neko.sfw.pat();
+            uri = uri.url;
+            ref.embed(message, text, "", uri);
+        } catch (err) {
+            console.log(err);
+        }
     }
 }
 
