59604e904dbcc3eb049f6856129d7345cd4c9332
moyshik7 <moyshik7@gmail.com>
2021-02-09 21:59:33 +0600

    added nsfw filter in .help commands

--- res/commands/help.js
+++ res/commands/help.js
@@ -5,7 +5,7 @@ const helpMessage = require("./resource/helpmessage.json");
 const app = {
     Help: (message, args) => {
         try {
-            let ho; //helpobject
+            let ho; //help object
             if (!args.length) {
                 args[0] = "help";
             }
@@ -15,13 +15,21 @@ const app = {
             } else {
                 ho = helpMessage["help"];
             }
-
-            let emb = new MessageEmbed()
-                .setColor("fd0061")
-                .setTitle(ho.name)
-                .setDescription(ho.content)
-                .addFields( /*{ name: "\u200B", value: "\u200B" },*/ { name: "__**Usage:**__", value: `${ho.usage || "N/A" }` }, { name: "__**Example:**__", value: `${ho.example || "N/A"}` })
-            message.channel.send(emb);
+            if(ho.nsfw == "true" && !message.channel.nsfw){
+	        let emb = new MessageEmbed()
+		    .setColor("fd0061")
+		    .setTitle("NSFW channel required")
+		    .setDescription("This term contains NSFW content.\nPlease use the command in an NSFW channel.")
+		message.channel.send(emb);
+	    } else {
+                let emb = new MessageEmbed()
+                    .setColor("fd0061")
+                    .setTitle(ho.name)
+                    .setDescription(ho.content)
+                    .addFields( /*{ name: "\u200B", value: "\u200B" },*/ { name: "__**Usage:**__", value: `${ho.usage || "N/A" }` }, { name: "__**Example:**__", value: `${ho.example || "N/A"}` })
+                message.channel.send(emb);
+	    }
+	    return(true);
         } catch (err) {
             console.log(err);
         }
