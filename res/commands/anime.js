8c1fb921f9affc713acb04cd602f31a5baf105bc
moyshik7 <moyshik7@gmail.com>
2021-03-07 20:45:23 +0600

    anime is non nsfw now

--- res/commands/anime.js
+++ res/commands/anime.js
@@ -8,10 +8,10 @@ const an = require("./anime.json");
 const all = {
     anime: async(message, args) => {
         try {
-	    if(!message.channel.nsfw){
+	    /*if(!message.channel.nsfw){
 		message.channel.send("Please use this command in any NSFW marked channel for safety");
 		return(false);
-	    }
+	    }*/
             let te;
             if (!args.length) {
                 te = ref.randArr(an.sfw);
