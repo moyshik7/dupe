b5343fd82154dcedc3c3cc38f27421491f1e2b56
moyshik7 <moyshik7@gmail.com>
2021-04-13 18:03:49 +0600

    adde wallpaper function in image.js

--- res/commands/image.js
+++ res/commands/image.js
@@ -1,1 +1,15 @@
-const Discord = require("doscord.js");
+const { MessageEmbed } = require("discord.js");
+const AllWallpapers = require("./resource/wallpapers.json");
+const ref = require("./../reference");
+const app = {
+    Wallpaper: (message) => {
+        let wallpaper = ref.randArr(AllWallpapers);
+        let emb = new MessageEmbed()
+            .setTitle("Here's your Wallpaper")
+            .setImage(wallpaper)
+        message.channel.send(emb);
+    }
+}
+
+
+module.exports = app;
